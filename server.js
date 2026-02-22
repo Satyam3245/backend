48469d3621888d83ba9df3836c1f002c7cdec93f
Satyam <mitravlakshay@gmail.com>
2026-02-22 22:33:48 +0530

    fix cors and port

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const cors = require("cors");
 const pool = require("./db/db")
 const app = express();
 app.use(cors());
-
+const PORT = process.env.PORT || 5000
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://backend-1-noh8.onrender.com",
   },
 });
 
@@ -291,6 +291,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(5000, () => {
+server.listen(PORT, () => {
   console.log("Server running on port 5000");
 });
